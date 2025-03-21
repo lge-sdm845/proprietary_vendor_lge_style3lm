57ead08a047154f7f30ec3aa1bba08d393a71f45
Tuan Anh <tuan73176@gmail.com>
2025-03-21 07:11:43 +0700

    style3lm: Extract proprietary firmware
    Change-Id: Ibf3ebc1aa4a4100f326b29befb5acef07781b249

--- Android.mk
+++ Android.mk
@@ -3,3 +3,26 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),style3lm)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,0e11614719dca1b53be05b54edfe3471455bb6b2)
+$(call add-radio-file-sha1-checked,radio/akmu.img,9058f5d646b0e453038d1f56526a79689905c01f)
+$(call add-radio-file-sha1-checked,radio/aop.img,6f8cfec93695022f2341e9d21566ec8ec0aab940)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,3adb43c82369c2194f2d4fd9b58014b579ba34bd)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,3921f9aebd600d5879f31e39d0fc16eb54d42972)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,149a7073dc25d9a4badd21cc27870a593a12ee7b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,23b79319d16cc796ea8cbaa15d048496dc9bdc35)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f9ef714b0062e599b73edaf56f4b5f341ec3fcf4)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,29a19d5daf251fd9ae368018af229a531e429df0)
+$(call add-radio-file-sha1-checked,radio/laf.img,5b1371232a8adc2e5056b097f2e5f00b6b4fbd71)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,57b587e1bf2d09335bdac6db18902d43dfe76449)
+$(call add-radio-file-sha1-checked,radio/mdtpsecapp.img,2bccbd2f38f15c13eb7d5a89fd9d85f595e23bc3)
+$(call add-radio-file-sha1-checked,radio/modem.img,69da879d27eb70f17d0d9748e3ffa2ed69a203a8)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,35fe83ba931a4153bc891bab1cdaefc2c4b77af8)
+$(call add-radio-file-sha1-checked,radio/storsec.img,19d377b7387aba1e0ed0c56f2d0627305b5600a6)
+$(call add-radio-file-sha1-checked,radio/tz.img,dc6f8d274e59c70fb6b050e1d54c56ffb17a9281)
+$(call add-radio-file-sha1-checked,radio/xbl.img,251f6e3dcdaf1531546191322cb82e6b8bacd5df)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,1adcdbca9c41628a9c9dc1b0327affce9fb800d7)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,23 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    akmu \
+    aop \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hyp \
+    keymaster \
+    laf \
+    mdtp \
+    mdtpsecapp \
+    modem \
+    qupfw \
+    storsec \
+    tz \
+    xbl \
+    xbl_config
